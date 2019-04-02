1745993710698051940ca9059a81266d04530ea5
Mateus Junges <contato@mateusjunges.com>
2019-04-01 21:28:17 -0300

    Added admin lte and vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ import Vue from 'vue'
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -30,8 +30,6 @@ const app = new Vue({
     el: '#app',
     data(){
         return{
-            test: true,
-            name: ''
         }
     }
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
 @extends('adminlte::page')
+@section('js')
+    <script src="{{ asset('js/app.js') }}"></script>
+@endsection
 @section('content')
-
+   <div id="app">
+   </div>
 @endsection
