891bfcb7e7c515702c78e18d78cfaf5f7620ef15
Mateus Junges <contato@mateusjunges.com>
2019-05-27 20:48:30 -0300

    Added Reason For Rescheduling model and migration

---
+++ database/migrations/2019_05_27_234337_create_reasons_for_rescheduling_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReasonsForReschedulingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reasons_for_rescheduling', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')
+                ->unique()
+                ->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reasons_for_rescheduling');
+    }
+}
