c7d97920331fc9bea15fa2bdaf5afe079ca94412
Mateus Junges <contato@mateusjunges.com>
2019-05-27 19:18:45 -0300

    Added Status model and migration

---
+++ app/Status.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HUAC;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Status extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'false';
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    protected $dates = ['deleted_at'];
+}
