ab68ae84d570055ec9a5da2d221aa1923da406de
Mateus Junges <contato@mateusjunges.com>
2019-05-27 19:08:28 -0300

    Added procedure model and migration

---
+++ database/migrations/2019_05_27_215957_create_procedures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProceduresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('procedures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60)->nullable(false);
+            $table->string('sus_code', 20)->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('procedures');
+    }
+}
