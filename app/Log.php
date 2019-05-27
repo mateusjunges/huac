ece9c06412ab2eb9c3eb7aeacaf75155f4f8f39f
Mateus Junges <contato@mateusjunges.com>
2019-05-27 20:39:35 -0300

    Added Log model and migration

---
+++ app/Log.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HUAC;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Log extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'logs';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'observation',
+        'surgery_id',
+        'status_id',
+        'user_id',
+    ];
+
+    protected $dates = ['deleted_at'];
+}
