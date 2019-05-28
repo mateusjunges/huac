da42aa47aba5807b0b0025ccc8d33c3f2d9c635e
Mateus Junges <contato@mateusjunges.com>
2019-05-27 21:11:16 -0300

    Added surgeries table columns

--- database/migrations/2019_05_27_224550_create_surgeries_table.php
+++ database/migrations/2019_05_27_224550_create_surgeries_table.php
@@ -15,6 +15,23 @@ class CreateSurgeriesTable extends Migration
     {
         Schema::create('surgeries', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('estimated_duration_time')->nullable(false);
+            $table->text('anesthetic_evaluation')->nullable();
+            $table->text('materials')->nullable(false);
+            $table->text('observation')->nullable();
+            $table->bigInteger('procedure_id', false, true);
+            $table->bigInteger('surgery_classification_id', false, true);
+            $table->bigInteger('patient_id', false, true);
+            $table->foreign('procedure_id')
+                ->references('id')
+                ->on('procedures');
+            $table->foreign('surgery_classification_id')
+                ->references('id')
+                ->on('surgery_classifications');
+            $table->foreign('patient_id')
+                ->references('id')
+                ->on('patients');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
