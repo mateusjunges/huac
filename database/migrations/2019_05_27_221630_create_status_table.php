564106cf0f49cfb540a3b68b9084462b80f45d3a
Mateus Junges <contato@mateusjunges.com>
2019-06-02 20:45:27 -0300

    Renamed migration

--- database/migrations/2019_05_27_221630_create_status_table.php
+++ database/migrations/2019_05_27_221630_create_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatusesTable extends Migration
+class CreateStatusTable extends Migration
 {
     /**
      * Run the migrations.
