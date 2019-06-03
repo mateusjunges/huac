3b0efb8827dd24c8ab6235036b9cc3bc3776bdda
Mateus Junges <contato@mateusjunges.com>
2019-06-03 15:11:41 -0300

    Added SQL triggers

---
+++ database/triggers/verify-patient-constraints-before-insert-on-patients.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION verify_patient_constraints()
+RETURNS TRIGGER AS 
+$BODY$
+	DECLARE countPatients integer := 0;
+BEGIN
+	-- Check for duplicate rows:
+	countPatients := COUNT(patients.*) FROM patients WHERE patients.medical_record = NEW.medical_record;
+	IF(countPatients > 0) THEN
+		RAISE NOTICE 'The given medical record is already registered!';
+		RETURN NULL;
+	END IF;
+	-- Check for null columns:
+	IF (NEW.name IS NULL) THEN
+		RAISE NOTICE 'The name attribute is required!';
+		RETURN NULL;
+	ELSIF (NEW.birthday_at IS NULL) THEN
+		RAISE NOTICE 'The birthday_at attribute is required!';
+		RETURN NULL;
+	ELSIF (NEW.mother_name IS NULL) THEN
+		RAISE NOTICE 'The mother_name  attribute is required!';
+		RETURN NULL;
+	ELSIF (NEW.gender IS NULL) THEN
+		RAISE NOTICE 'The gender attribute is required!';
+		RETURN NULL;
+	ELSIF (NEW.medical_record IS NULL) THEN
+		RAISE NOTICE 'The medical_record attribute is required!';
+		RETURN NULL;
+	END IF;
+	-- Check for minstring length
+	IF(SELECT LENGTH(NEW.name) NOT BETWEEN 1 AND 70)THEN
+		RAISE NOTICE 'The name must have between 1 and 70 characters';
+		RETURN NULL;
+	ELSIF(SELECT LENGTH(NEW.mother_name) NOT BETWEEN 1 AND 70) THEN
+		RAISE NOTICE 'The mother_name attribute must have between 1 and 70 characters';
+		RETURN NULL;
+	ELSIF (SELECT LENGTH(NEW.medical_record) NOT BETWEEN 1 AND 15) THEN
+		RAISE NOTICE 'The medical record must have between 1 and 15 characters';
+		RETURN NULL;
+	END IF;
+	
+	-- Check for gender in gender enum:
+	IF((NEW.gender <> 'M') AND (NEW.gender <> 'F') AND (NEW.gender <> 'O')) THEN
+		RAISE NOTICE 'The gender attribute must be one of the following characters: M, F, O';
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_patients_contraints_before_insert_on_patients BEFORE INSERT ON "patients"
+FOR EACH ROW EXECUTE PROCEDURE verify_patient_constraints();
---
+++ database/triggers/verify-status-constraints-before-insert-on-status.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION verify_status_constraints()
+RETURNS TRIGGER AS 
+$BODY$
+	DECLARE countStatus integer := 0;
+BEGIN
+	-- Check for duplicate rows:
+	countStatus := COUNT(status.*) FROM status WHERE status.name = NEW.name;
+	IF (countStatus > 0)
+	THEN 
+		RAISE NOTICE 'The given name is already in use!';
+		RETURN NULL;
+	END IF;
+	
+	-- Check for required columns:
+	IF(NEW.name IS NULL)
+	THEN
+		RAISE NOTICE 'The name attribute is required!';
+		RETURN NULL;
+	ELSIF (NEW.description IS NULL)
+	THEN
+		RAISE NOTICE 'The description attribute is required!';
+		RETURN NULL;
+	END IF;
+	
+	-- Check for string length:
+	IF (SELECT LENGTH(NEW.name) NOT BETWEEN 1 AND 20)
+	THEN 
+		RAISE NOTICE 'The name must have between 1 and 20 characters';
+		RETURN NULL;
+	ELSIF (SELECT LENGTH(NEW.description) <= 5)
+	THEN
+		RAISE NOTICE 'The description must have more than 5 characters!';
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_status_constraints_before_insert_on_status BEFORE INSERT ON "status"
+FOR EACH ROW EXECUTE PROCEDURE verify_status_constraints();
