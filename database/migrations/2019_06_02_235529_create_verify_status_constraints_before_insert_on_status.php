da2193c63350c76990c72037023cf83654769a2d
Mateus Junges <contato@mateusjunges.com>
2019-06-04 10:28:41 -0300

    Renamed trigger class

--- database/migrations/2019_06_02_235529_create_verify_status_constraints_before_insert_on_status.php
+++ database/migrations/2019_06_02_235529_create_verify_status_constraints_before_insert_on_status.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class VerifyStatusConstraintsBeforeInsertOnStatus extends Migration
+class CreateVerifyStatusConstraintsBeforeInsertOnStatus extends Migration
 {
     /**
      * Run the migrations.
