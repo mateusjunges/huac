755fb0c9376e352ae4909cb9ee977c6ab819f416
Mateus Junges <contato@mateusjunges.com>
2019-06-08 20:26:01 -0300

    Added SQL file that create anesthetics trigger

---
+++ database/triggers/verify-anesthetics-constraints.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION verify_anesthetics_constraints()
+RETURNS TRIGGER AS
+$BODY$
+BEGIN
+	-- Check for null columns:
+	IF (NEW.name IS NULL)
+	THEN 
+		RAISE EXCEPTION 'The "name" attribute is required!';
+		RETURN NULL;
+	END IF;
+	
+	-- Check for string length:
+	IF (SELECT LENGTH(NEW.name) NOT BETWEEN 1 AND 60)
+	THEN
+		RAISE EXCEPTION 'The "name" attribute must have between 1 and 60 characters!';
+		RETURN NULL;
+	END IF;
+	
+	RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_anesthetics_constraints BEFORE INSERT ON anesthetics
+FOR EACH ROW EXECUTE PROCEDURE verify_anesthetics_constraints();
