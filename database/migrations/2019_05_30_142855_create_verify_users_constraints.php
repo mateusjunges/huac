263d2dae77d7775d8b31cff6ab372c1fbbc88aef
Mateus Junges <contato@mateusjunges.com>
2019-06-08 19:51:24 -0300

    Renamed users trigger

--- database/migrations/2019_05_30_142855_create_verify_users_constraints.php
+++ database/migrations/2019_05_30_142855_create_verify_users_constraints.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVerifyNullColumnsOnUsersTrigger extends Migration
+class CreateVerifyUsersConstraints extends Migration
 {
     /**
      * Run the migrations.
