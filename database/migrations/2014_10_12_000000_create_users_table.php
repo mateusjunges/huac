e442aef47cb9fcf85d2f36ab7e7c04f1b780d610
Mateus Junges <contato@mateusjunges.com>
2019-06-16 02:21:48 -0300

    Update max length for user table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique()->nullable(false);
+            $table->string('name', 70);
+            $table->string('email', 70)->unique();
+            $table->string('username', 30)->unique()->nullable(false);
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
