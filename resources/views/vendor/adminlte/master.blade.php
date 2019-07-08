4d4095e160d4b00d22ca53b386fd8d53f8b5edd7
Mateus Junges <contato@mateusjunges.com>
2019-07-08 02:33:35 -0300

    Added meta lang to master template

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
+    <meta name="@lang('pt-br')">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,8 @@
 @endsection
 @section('content')
    <div id="app">
+       <form action="">
+           <create-surgery></create-surgery>
+       </form>
    </div>
 @endsection
