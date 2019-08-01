ba1fc3a01e6443bd97a5f2dcec2f94cdbd6a50fe
Mateus Junges <contato@mateusjunges.com>
2019-08-01 19:59:29 -0300

    Added SGI routes to a dedicated file

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,12 @@ use HUAC\Http\Controllers\Surgery\SurgeryController;
 /*
  * Rotas padrões do pacote SGIAuthorizer para login e exibição dos usuários logados.
  */
-Route::get(config('sgiauthorizer.app.loginRoute'), [
-    'as' => 'login',
-    'uses' => '\Uepg\SGIAuthorizer\Auth\Controllers\LoginController@getLogin'
-]);
-Route::post(config('sgiauthorizer.app.loginRoute'), 'HUAC\Http\Controllers\Auth\LoginController@login');
-Route::get('/logout', [
-    'as' => 'logout',
-    'uses' => 'HUAC\Http\Controllers\Auth\LoginController@logout'
-]);
-
-Route::group(['middleware' => 'sgiauth'], function() {
-    Route::any(config('sgiauthorizer.app.userInfoRoute'),
-        [
-            'uses' => '\Uepg\SGIAuthorizer\Auth\Controllers\DisplayUserInfoController@userInfo'
-        ])->name('userinfo');
-});
+include_once('sgi-authorizer/web.php');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::group(['middleware' => 'sgi-auth'], function (){
    Route::get('/home', HomeController::class);
