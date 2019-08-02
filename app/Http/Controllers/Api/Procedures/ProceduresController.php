5ed1b971c8527f2607ddc39c788fd5c2de5f9426
Mateus Junges <contato@mateusjunges.com>
2019-08-01 21:10:57 -0300

    Added a procedure controller to be used for api methods

---
+++ app/Http/Controllers/Api/Procedures/ProceduresController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HUAC\Http\Controllers\Api\Procedures;
+
+use HUAC\Http\Resources\ProceduresResource;
+use HUAC\Procedure;
+
+class ProceduresController
+{
+    /**
+     * Return all the stored procedures.
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function all()
+    {
+        return ProceduresResource::collection(
+            Procedure::all()
+        );
+    }
+
+    /**
+     * Return the specified procedure
+     * @param $id
+     * @return ProceduresResource
+     */
+    public function find($id)
+    {
+        return new ProceduresResource(
+            Procedure::find($id)
+        );
+    }
+}
