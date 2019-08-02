2017463450196523949d24e350b4436a2869a6c1
Mateus Junges <contato@mateusjunges.com>
2019-08-01 21:14:06 -0300

    Added api routes for procedures

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use HUAC\Http\Controllers\Api\Procedures\ProceduresController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth-api')->group(function () {
+    Route::get('procedures', [ProceduresController::class, 'all'])->name('api.procedures.index');
+    Route::get('procedures/{procedure}', [ProceduresController::class, 'find'])->name('api.procedures.find');
+});
+
+
