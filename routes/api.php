b9f6670f8098d189140fdc20872fbd5353b9b82e
Mateus Junges <contato@mateusjunges.com>
2019-08-05 11:30:15 -0300

    Added routes for surgeons

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use HUAC\Http\Controllers\Api\Procedures\ProceduresController;
 use HUAC\Http\Controllers\Api\Anesthetics\AnestheticsController;
+use HUAC\Http\Controllers\Api\Surgeons\SurgeonsController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -11,7 +12,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::middleware(['auth:api'])->group(function () {
     Route::get('procedures', [ProceduresController::class, 'all'])->name('api.procedures.index');
     Route::get('procedures/{procedure}', [ProceduresController::class, 'find'])->name('api.procedures.find');
+
     Route::get('anesthetics', [AnestheticsController::class, 'all'])->name('api.anesthetics.all');
     Route::get('anesthetics/{anesthesia}', [AnestheticsController::class, 'find'])->name('api.anesthetics.find');
+
+    Route::get('surgeons', [SurgeonsController::class, 'all'])->name('api.surgeons.all');
+    Route::get('surgeons/{surgeon}', [SurgeonsController::class, 'find'])->name('api.surgeons.find');
 });
 
