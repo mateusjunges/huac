c8560d8520422f625d7d1437c76395e759706d86
Mateus Junges <contato@mateusjunges.com>
2019-08-06 10:28:36 -0300

    Wip: crud for groups

---
+++ resources/js/components/ACL/CreateGroupsComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="box box-success">
+            <div class="box-header">
+
+            </div>
+            <div class="box-body">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CreateGroupsComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/ACL/groups/create.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+@section('css')
+@endsection
+@section('js')
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1 class="text-center">Cadastro de grupos de permissões</h1>
+    </div>
+</div>
+@endsection
