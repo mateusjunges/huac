c1ea0c36ed5290410123d16efdb90f3a5372d176
Mateus Junges <contato@mateusjunges.com>
2019-08-11 14:54:32 -0300

    Fix remove group functionality

--- public/js/ACL/users/groups/remove-group.js
+++ public/js/ACL/users/groups/remove-group.js
@@ -17,29 +17,38 @@ $(document).ready(function () {
      * Revoke user groups.
      */
     dataTable.on('click', 'button.delete', function () {
-        let user = $(this).data('user_id');
-        let group = $(this).data('id');
-        var tr = $(this).closest('tr');
-        $.ajax({
-            url: `/api/users/${user}/remove-group`,
-            method: 'post',
-            headers:  headers,
-            data: {
-                _method: 'delete',
-                groups: group,
-            },
-            success: function (response) {
-                swal({
-                    icon: response.icon,
-                    title: response.title,
-                    text: response.text,
-                    timer: response.timer,
+        swal({
+            icon: 'warning',
+            title: 'Tem certeza?',
+            text: 'Deseja remover este grupo?',
+            buttons: ['Não', 'Sim'],
+        }).then((response) => {
+            if (response === true) {
+                let user = $(this).data('user_id');
+                let group = $(this).data('id');
+                var tr = $(this).closest('tr');
+                $.ajax({
+                    url: `/api/users/${user}/remove-group`,
+                    method: 'post',
+                    headers:  headers,
+                    data: {
+                        _method: 'delete',
+                        groups: group,
+                    },
+                    success: function (response) {
+                        swal({
+                            icon: response.icon,
+                            title: response.title,
+                            text: response.text,
+                            timer: response.timer,
+                        });
+                        if (response.code === 200){
+                            tr.fadeOut(400, function () {
+                                tr.remove();
+                            });
+                        }
+                    }
                 });
-                if (response.code === 200){
-                    tr.fadeOut(400, function () {
-                        tr.remove();
-                    });
-                }
             }
         })
     });
