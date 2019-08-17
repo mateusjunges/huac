e55d3a62c6d6c653b69d5bacf3ac735eaabd0dc2
Mateus Junges <contato@mateusjunges.com>
2019-08-17 19:42:05 -0300

    Add index view for surgeries. Finished surgeries create/store methods

---
+++ app/Models/Views/Surgeries.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HUAC\Models\Views;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Surgeries extends Model
+{
+    protected $table = 'show_surgeries';
+}
--- resources/views/surgeries/index.blade.php
+++ resources/views/surgeries/index.blade.php
@@ -1,13 +1,41 @@
 @extends("adminlte::page")
 @section("title", "Cirurgias cadastradas")
 @section('css')
+    <style>
+        .table {
+            width: 100% !important;
+        }
+    </style>
 @endsection
 @section('js')
+    <script src="{{ asset('js/surgeries/index.js') }}"></script>
 @endsection
 @section('content')
     <div class="row">
         <div class="col-md-12">
             <h1 class="text-center">Cirurgias cadastradas</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-hover table-responsive dataTable">
+                <thead>
+                    <tr>
+                        <th>Paciente</th>
+                        <th>Prontuário</th>
+                        @can('surgeries.edit')
+                            <th>Editar</th>
+                        @endcan
+                        @can('surgeries.delete')
+                            <th>Excluir</th>
+                        @endcan
+                        <th>Médico principal</th>
+                        <th>Procedimento</th>
+                        <th>Status</th>
+                        <th>Agendamento</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
     </div>
 @endsection
