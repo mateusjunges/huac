c6e3b910aac1bbc9d766c497debd18f848f6ea05
Mateus Junges <contato@mateusjunges.com>
2019-08-30 09:27:17 -0300

    Addd patients request

---
+++ app/Http/Requests/PatientRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace HUAC\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
+
+class PatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = 'NULL';
+        if (Route::getCurrentRoute()->parameters() != null)
+            $id = Route::getCurrentRoute()->parameters()['patient'];
+        return [
+            'name'           => 'required|min:7|full_name',
+            'medical_record' => 'required|unique:patients,medical_record,'.$id,
+            'mother_name'    => 'required|min:7|full_name',
+            'birthday_at'    => 'required|date',
+            'gender'         => 'required|in:M,O,F'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name'           => 'nome',
+            'medical_record' => 'prontuário',
+            'mother_name'    => 'nome da mãe',
+            'birthday_at'    => 'data de nascimento',
+            'gender'         => 'gênero'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            '*.required'       => 'O campo :attribute é obrigatório!',
+            '*.full_name'      => 'Você deve informar o nome completo!',
+            '*.min'            => 'O campo :attribute requer :min caracteres!',
+            'birthday_at.date' => 'Informe uma data válida!',
+            'gender.in'        => 'Selecione uma das opções da lista!',
+            '*unique'          => 'Este :attribute já está cadastrado para outro paciente!',
+        ];
+    }
+}
