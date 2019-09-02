7465c585a08c2a2b9e189b14803bb5a9440ec349
Mateus Junges <contato@mateusjunges.com>
2019-09-02 08:25:45 -0300

    Fix event duration before drag to the full calendar

--- public/js/scheduling/make-events-draggable.js
+++ public/js/scheduling/make-events-draggable.js
@@ -1,8 +1,15 @@
 $('#external-events .fc-event').each(function() {
     // store data so the calendar knows to render an event upon drop
+    let estimated = $(this).data('estimated');
+    if (estimated < 10)
+        estimated = '0' + estimated.toString();
+    else
+        estimated = estimated.toString();
+
     $(this).data('event', {
         title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true // maintain when user navigates (see docs on the renderEvent method)
+        stick: true, // maintain when user navigates (see docs on the renderEvent method)
+        duration: estimated + ':00:00',
     });
     // make the event draggable using jQuery UI
     $(this).draggable({
