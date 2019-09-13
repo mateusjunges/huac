380558f3d40520b1e2bb6584346ae2f52e13af74
Mateus Junges <contato@mateusjunges.com>
2019-09-13 10:23:14 -0300

    wip

--- app/Http/Controllers/Reports/AverageProcedureDuration.php
+++ app/Http/Controllers/Reports/AverageProcedureDuration.php
@@ -2,7 +2,24 @@
 
 namespace HUAC\Http\Controllers\Reports;
 
+use HUAC\Models\Procedure;
+use Illuminate\Http\Request;
+
 class AverageProcedureDuration
 {
-
+    public function __invoke(Request $request)
+    {
+        if ($request->has('procedure')){
+            return null;
+        } else {
+            $procedures = Procedure::has('surgeries.events')->get();
+            $data = [];
+            $procedures->map(function ($procedure) use ($data) {
+                $data[$procedure->id] = [
+                    'average_duration' => null,
+                ];
+//                $averageDuration =
+            });
+        }
+    }
 }
