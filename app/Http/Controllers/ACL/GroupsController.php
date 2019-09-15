654f0e8126b84bf7f06e695f391666c7f35f1c10
Mateus Junges <contato@mateusjunges.com>
2019-09-15 14:44:29 -0300

    Added permissions restrictions to GroupsController

--- app/Http/Controllers/ACL/GroupsController.php
+++ app/Http/Controllers/ACL/GroupsController.php
@@ -4,6 +4,8 @@ namespace HUAC\Http\Controllers\ACL;
 
 use HUAC\Http\Requests\GroupRequest;
 use HUAC\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Junges\ACL\Http\Models\Group;
 use Junges\ACL\Http\Models\Permission;
 
@@ -12,10 +14,20 @@ class GroupsController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
+        if (Gate::denies('groups.index')){
+            $message = array(
+              'title' => 'Acesso negado!',
+              'text' => 'Você não possui permissão para acessar esta área do sistema!',
+              'type' => 'warning',
+            );
+            session()->flash('message', $message);
+            return redirect()->back();
+        };
+
         $groups = Group::all();
         return view('ACL.groups.index')->with([
             'groups' => $groups
@@ -25,10 +37,20 @@ class GroupsController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
+        if (Gate::denies('groups.create')) {
+            $message = array(
+                'title' => 'Acesso negado!',
+                'text' => 'Você não possui permissão para acessar esta área do sistema!',
+                'type' => 'warning',
+            );
+            session()->flash('message', $message);
+            return redirect()->back();
+        }
+
         $permissions = Permission::all();
         return view('ACL.groups.create')->with([
             'permissions' => $permissions
@@ -39,10 +61,20 @@ class GroupsController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  GroupRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(GroupRequest $request)
     {
+        if (Gate::denies('groups.create')) {
+            $message = array(
+                'title' => 'Acesso negado!',
+                'text' => 'Você não possui permissão para acessar esta área do sistema!',
+                'type' => 'warning',
+            );
+            session()->flash('message', $message);
+            return redirect()->back();
+        }
+
         $group = Group::create($request->except('permissions'));
 
         collect($request->input('permissions'))->map(function ($permission) use ($group) {
@@ -61,24 +93,22 @@ class GroupsController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  Group $group
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Group $group)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  Group $group
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit(Group $group)
     {
+        if (Gate::denies('groups.update')) {
+            $message = array(
+                'title' => 'Acesso negado!',
+                'text' => 'Você não possui permissão para acessar esta área do sistema!',
+                'type' => 'warning',
+            );
+            session()->flash('message', $message);
+            return redirect()->back();
+        }
         $groupPermissions = $group->permissions()->get();
         $permissions = Permission::all();
         return view('ACL.groups.edit')->with([
@@ -91,12 +121,22 @@ class GroupsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  Group $group
-     * @return \Illuminate\Http\Response
+     * @param GroupRequest $request
+     * @param Group $group
+     * @return Response
      */
     public function update(GroupRequest $request, Group $group)
     {
+        if (Gate::denies('groups.update')) {
+            $message = array(
+                'title' => 'Acesso negado!',
+                'text' => 'Você não possui permissão para acessar esta área do sistema!',
+                'type' => 'warning',
+            );
+            session()->flash('message', $message);
+            return redirect()->back();
+        }
+        
         $group = $group->syncPermissions($request->permissions);
 
         $message = array(
