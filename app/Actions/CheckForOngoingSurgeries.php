c9bc7a1ab45a7c316c3af2ce072ccb8965b5416b
Mateus Junges <contato@mateusjunges.com>
2019-09-24 12:57:45 -0300

    Added check for on going surgeries action

---
+++ app/Actions/CheckForOngoingSurgeries.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HUAC\Actions;
+
+class CheckForOngoingSurgeries
+{
+    /**
+    * CheckForOngoingSurgeries constructor
+    */
+    public function __construct()
+    {
+        //
+    }
+
+    public function execute()
+    {
+        //
+    }
+}
